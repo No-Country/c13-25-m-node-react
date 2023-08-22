8231fb561d7f7a0d39800877ea9f0a175d1cb6f4
Adrian <esteban.adrian.gonz@gmail.com>
2023-08-22 17:42:29 -0300

    Integrated db connection with mongoose dependency

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,13 +1,16 @@
 import http from 'http'
 import app from './app'
+// import { connectDB } from './database/database'
 
 const PORT = 8000
 const server = http.createServer(app)
 
-const startServer = (): void => {
+const startServer = async (): Promise<void> => {
+  // descomentar junto al import cuando se inicie a usar
+  // await connectDB()
   server.listen(PORT, () => {
     console.log(`Listening on port:${PORT}`)
   })
 }
 
-startServer()
+void startServer()
