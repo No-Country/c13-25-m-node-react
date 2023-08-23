a0228e4d683424c233b22b121c9bbedf2dcc137c
AndyCambra <andycambra@gmail.com>
2023-08-23 13:32:23 -0300

    feat singup with validations

--- client/src/routes/AppRouter/index.tsx
+++ client/src/routes/AppRouter/index.tsx
@@ -1,10 +1,12 @@
 import { Home } from '@/pages/Home'
 import { Route, Routes } from 'react-router-dom'
 import LoginLayout from '@/pages/Login/LoginLayout'
+import { SingnUp } from '@/pages/SignUp'
 
 export const AppRouter = () => {
   return (
     <Routes>
+      <Route path="/signup" element={<SingnUp />} />
       <Route path="/" element={<Home />} />
       <Route path="/login" element={<LoginLayout />} />
     </Routes>
