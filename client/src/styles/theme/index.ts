87e6dad19f9a579081391231e46bfed8d364ba57
Francoheredia <francofalcondev@gmail.com>
2023-08-23 19:45:25 -0300

    feat header add component and logic

--- client/src/styles/theme/index.ts
+++ client/src/styles/theme/index.ts
@@ -1,6 +1,38 @@
 import { extendTheme } from '@chakra-ui/react'
 
-const customTheme = {}
+const customTheme = {
+  colors: {
+    viegogreen: '#319795', // color personalizado
+    navbarcolor: {
+      500: '#282C37', // color personalizado
+    },
+  },
+  components: {
+    Button: {
+      variants: {
+        LogsButtons: {
+          borderColor: '#319795', // Color del borde
+          color: '#319795', // Color del texto
+          _focus: {
+            border: '2px solid',
+            borderColor: '#319795', // Color de fondo al pasar el mouse
+            color: '#319795', // Color del texto al pasar el mouse
+          },
+        },
+        LoginButton: {
+          borderColor: '#7E7E7E',
+          color: '#FFFFFF',
+          bg: '#7E7E7E',
+          _focus: {
+            border: '2px solid',
+            borderColor: '#319795',
+            bg: '#319795',
+          },
+        },
+      },
+    },
+  },
+}
 
 const theme = extendTheme(customTheme)
 
