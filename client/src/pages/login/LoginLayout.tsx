5f587541cd4d7a73763edcba5ec9ca228c1d43ef
Leandroni1983 <leandronicolas1983@gmail.com>
2023-08-22 22:15:24 -0300

    feat login : add login logica

---
+++ client/src/pages/login/LoginLayout.tsx
@@ -0,0 +1,13 @@
+import Navbar from "@/components/Navbar"
+import { Box } from "@chakra-ui/react"
+import Login from "./Login"
+
+
+export default function LoginLayout() {
+    return (
+        <Box>
+            <Navbar />
+            <Login />
+        </Box>
+    )
+}
--- client/src/routes/AppRouter/index.tsx
+++ client/src/routes/AppRouter/index.tsx
@@ -1,10 +1,12 @@
 import { Home } from '@/pages/Home'
+import LoginLayout from '@/pages/login/LoginLayout'
 import { Route, Routes } from 'react-router-dom'
 
 export const AppRouter = () => {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
+      <Route path="/login" element={<LoginLayout />} />
     </Routes>
   )
 }
