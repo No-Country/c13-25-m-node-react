756086ce193b9e324dd74b5aaf3b98aaf1faa35c
Leandroni1983 <leandronicolas1983@gmail.com>
2023-08-22 22:49:37 -0300

    feat login : add login logica

--- client/src/routes/AppRouter/index.tsx
+++ client/src/routes/AppRouter/index.tsx
@@ -1,10 +1,12 @@
 import { Home } from '@/pages/Home'
 import { Route, Routes } from 'react-router-dom'
+import LoginLayout from '@/pages/Login/LoginLayout'
 
 export const AppRouter = () => {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
+      <Route path="/login" element={<LoginLayout />} />
     </Routes>
   )
 }
