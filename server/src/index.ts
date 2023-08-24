8d2652a0f7ea39b21a4866e61026867839a74659
Adrian <esteban.adrian.gonz@gmail.com>
2023-08-24 17:05:51 -0300

    feat: DB up

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,13 +1,12 @@
 import http from 'http'
 import app from './app'
-// import { connectDB } from './database/database'
+import { connectDB } from './database/database'
 
 const PORT = 8000
 const server = http.createServer(app)
 
 const startServer = async (): Promise<void> => {
-  // descomentar junto al import cuando se inicie a usar
-  // await connectDB()
+  await connectDB()
   server.listen(PORT, () => {
     console.log(`Listening on port:${PORT}`)
   })
