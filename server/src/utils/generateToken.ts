ab548de9f7c8e92a9cc0bfadd5fff038bf578a56
Adrian <esteban.adrian.gonz@gmail.com>
2023-08-28 20:02:26 -0300

    add: token generator util

---
+++ server/src/utils/generateToken.ts
@@ -0,0 +1,5 @@
+import jwt from 'jsonwebtoken'
+
+export const generateToken = (username: string, email: string): string => {
+  return jwt.sign({ username, email }, process.env.JWT_SECRET as string, { expiresIn: '2m' })
+}
