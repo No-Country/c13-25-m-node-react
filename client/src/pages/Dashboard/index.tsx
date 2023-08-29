589b98ca8c7494775f0d7a57f302f76c5ab85316
Francoheredia <francofalcondev@gmail.com>
2023-08-28 22:58:35 -0300

    fix changes before conflicts

--- client/src/pages/Dashboard/index.tsx
+++ client/src/pages/Dashboard/index.tsx
@@ -1,4 +1,4 @@
-import { Box, Grid, List, Text } from '@chakra-ui/react'
+import { Box, List, Text } from '@chakra-ui/react'
 
 import {
   LiaNewspaperSolid,
@@ -9,25 +9,22 @@ import {
 
 import { FeedComponent } from './components/FeedComponent/index.tsx'
 import { SideButton } from './components/SideButton/index.tsx'
+
 export const Dashboard = () => {
   return (
     <Box p={4} height="100vh" bg={'#1E1E1E'}>
-      <Grid templateColumns="2fr 8fr 2fr" gap={4} height="100%">
-        <Box bg="#282C37" p={4}>
-          <List spacing={1}>
-            <SideButton icon={LiaHouseDamageSolid} name="Home" />
-            <SideButton icon={LiaNewspaperSolid} name="Noticias" />
-            <SideButton icon={LiaUsersSolid} name="Mi equipo" />
-            <SideButton icon={LiaRocketchat} name="Chat" />
-          </List>
-        </Box>
-        <Box>
-          <FeedComponent />
-        </Box>
-        <Box bg="#282C37" p={4}>
-          <Text>Componente recomendados</Text>
-        </Box>
-      </Grid>
+      <List spacing={1}>
+        <SideButton icon={LiaHouseDamageSolid} name="Home" />
+        <SideButton icon={LiaNewspaperSolid} name="Noticias" />
+        <SideButton icon={LiaUsersSolid} name="Mi equipo" />
+        <SideButton icon={LiaRocketchat} name="Chat" />
+      </List>
+
+      <FeedComponent />
+
+      <Box bg="#282C37" p={4}>
+        <Text>Componente recomendados</Text>
+      </Box>
     </Box>
   )
 }
