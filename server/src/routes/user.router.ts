a1311af316b2b55aa3210355ae0cb67d37c03794
Adrian <esteban.adrian.gonz@gmail.com>
2023-08-30 22:14:12 -0300

    feat: login route created

--- server/src/routes/user.router.ts
+++ server/src/routes/user.router.ts
@@ -1,10 +1,14 @@
 import express from 'express'
 import { registerController } from '../controllers/user/register.controller'
+import { loginController } from '../controllers/user/login.controller'
 import { signValidator } from '../middleware/signValidator.middleware'
+import { loginValidator } from '../middleware/loginValidator.middleware'
 const userRouter = express.Router()
 
 // CREATE NEW USER
 userRouter.post('/register', signValidator, registerController)
 // UPDATE EXISTING USER
+// LOGIN USER
+userRouter.post('/login', loginValidator, loginController)
 
 export default userRouter
