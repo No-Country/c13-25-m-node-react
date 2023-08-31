00f955ae95118993a6cf9f51c2d456b6e5367255
Maurizio <mauriziodm91@gmail.com>
2023-08-30 21:07:18 -0300

    feat: Created updateUser method, controller and service

---
+++ server/src/services/user/updateUser.service.ts
@@ -0,0 +1,18 @@
+import User from '../../models/User'
+import { Document, Types } from 'mongoose'
+import { User as IUser } from '../../../types'
+import { hashPassword } from '../../utils/auth/hashPassword'
+
+export const updateUserService = async (
+  id: Types.ObjectId,
+  data: IUser
+): Promise<Document | null> => {
+  const { password } = data
+  return (await User.findByIdAndUpdate(
+    id,
+    {
+      password: await hashPassword(password)
+    },
+    { new: true }
+  ).select('-__v')) as Document | null
+}
