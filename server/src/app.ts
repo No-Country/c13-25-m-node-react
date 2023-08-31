12eae9d04ad72f80fb5dd2a7b871c815ca6f012b
Adrian <esteban.adrian.gonz@gmail.com>
2023-08-30 22:10:05 -0300

    add: cors & passport integrated

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,9 +1,17 @@
 import 'dotenv/config'
 import express from 'express'
+import cors from 'cors'
 import api from './routes/api'
+import passport from 'passport'
+import { passportMiddleware } from './middleware/passportJwt.middleware'
 const app = express()
 
+app.use(cors({
+  origin: '*'
+}))
 app.use(express.json())
+app.use(passport.initialize())
+passport.use(passportMiddleware)
 app.use('/v1', api)
 
 app.get('/', (_req, res) => res.json({ message: 'Welcome to out API' }))
