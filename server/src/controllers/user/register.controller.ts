5af66127e0e714ead1771e948a5f1d717964861c
Adrian <esteban.adrian.gonz@gmail.com>
2023-08-31 11:49:29 -0300

    feat: added token in response

--- server/src/controllers/user/register.controller.ts
+++ server/src/controllers/user/register.controller.ts
@@ -1,13 +1,30 @@
 import { Request, Response } from 'express'
 import { checkIfExists } from '../../utils/auth/userExists'
 import { createUserService } from '../../services/user/createUser.service'
+import { generateToken } from '../../utils/auth/generateToken'
+import { User as IUser } from '../../../types'
 
-export const registerController = async (req: Request, res: Response): Promise<Response | JSON> => {
+export const registerController = async (req: Request, res: Response): Promise<Response> => {
+  let user
+  let token
   try {
     if (await checkIfExists(req.body.email)) return res.status(400).json({ message: 'El email se encuentra registrado' })
 
-    const newUser = await createUserService(req.body)
-    return res.json({ message: 'Usuario creado', user: newUser })
+    const newUser: IUser | string = await createUserService(req.body)
+    if (typeof newUser !== 'string') {
+      token = generateToken(newUser.email, newUser.password)
+      user = {
+        _id: newUser._id,
+        username: newUser.username,
+        email: newUser.email,
+        photourl: newUser.photourl
+      }
+    }
+    return res.json({
+      message: 'Usuario creado',
+      user,
+      token
+    })
   } catch (error) {
     let errorMessage
     if (error instanceof Error) errorMessage = error.message
