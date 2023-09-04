031869779393cded679d51fb3abc289dbeb3c562
AndyCambra <andycambra@gmail.com>
2023-09-04 16:14:46 -0300

    fit profile

--- client/src/routes/AppRouter/index.tsx
+++ client/src/routes/AppRouter/index.tsx
@@ -1,3 +1,4 @@
+import { Profile } from '@/features/ui/Profile'
 import { Dashboard, SingnUp, Login, Onboarding } from '@/pages'
 import { Route, Routes } from 'react-router-dom'
 
@@ -9,6 +10,7 @@ export const AppRouter = () => {
       <Route path="/" element={<Onboarding />} />
       <Route path="/login" element={<Login />} />
       <Route path="/dashboard" element={<Dashboard />} />
+      <Route path="/profile" element={<Profile />} />
     </Routes>
   )
 }
