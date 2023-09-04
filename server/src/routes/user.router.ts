3659531e825953c69c12be68524a1b32ed069276
Adrian <esteban.adrian.gonz@gmail.com>
2023-09-04 11:10:48 -0300

    feat: logout route included

--- server/src/routes/user.router.ts
+++ server/src/routes/user.router.ts
@@ -8,6 +8,7 @@ import { signValidator } from '../middleware/signValidator.middleware'
 import { updateUserController } from '../controllers/user/updateUser.controller'
 import { loginValidator } from '../middleware/loginValidator.middleware'
 import passport from 'passport'
+import { logoutController } from '../controllers/user/logout.controller'
 const userRouter = express.Router()
 
 // GET ALL USERS
@@ -21,6 +22,9 @@ userRouter.post('/register', signValidator, registerController)
 
 // LOGIN USER
 userRouter.post('/login', loginValidator, loginController)
+
+// LOGOUT USER
+userRouter.post('/logout', passport.authenticate('jwt', { session: false }), logoutController)
 
 // UPDATE EXISTING USER
 userRouter.put('/:id', passport.authenticate('jwt', { session: false }), signValidator, updateUserController)
